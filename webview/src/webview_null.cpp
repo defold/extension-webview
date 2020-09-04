bb922d22f4b52e3dcacba247ce83417f4d81f8e0
thetrung <deulamco@gmail.com>
2020-09-04 16:31:55 +0700

    add support for macOS
    removed unnecessary  parts
    
    remove stuff
    
    cut down stuff
    
    rename -> _darwin.mm

--- webview/src/webview_null.cpp
+++ webview/src/webview_null.cpp
@@ -1,4 +1,4 @@
-#if !defined(DM_PLATFORM_ANDROID) && !defined(DM_PLATFORM_IOS)
+#if !defined(DM_PLATFORM_ANDROID) && !defined(DM_PLATFORM_IOS) && !defined(DM_PLATFORM_OSX)
 extern "C" void WebViewExternal()
 {
 
